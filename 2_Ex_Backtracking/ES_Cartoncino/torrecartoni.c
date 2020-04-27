6d25d00a5d004cf1a8452786570c874d32551649
mich2k <miky3848@gmail.com>
2020-04-27 20:51:03 +0200

    adjusted main

--- 2_Ex_Backtracking/ES_Cartoncino/torrecartoni.c
+++ 2_Ex_Backtracking/ES_Cartoncino/torrecartoni.c
@@ -1,8 +1,3 @@
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdio.h>
-
 #include "torrecartoni.h"
 
 typedef struct{
@@ -71,4 +66,20 @@ void TorreCartoni(cartone *c, int n)
     free(usati);
     free(vcurr.c_ids);
     free(vbest.c_ids);
+}
+
+int main(void) // main / input
+{
+    cartone c[] = {
+        {.p = 25, .a = 20, .l = 50},
+        {.p = 10, .a = 10, .l = 8},
+        {.p = 9, .a = 3, .l = 5},
+        {.p = 5, .a = 20, .l = 200},
+        {.p = 10, .a = 10, .l = 202},
+    };
+    int n = sizeof(c) / sizeof(*c);
+    TorreCartoni(c, n);
+    // 4 digits 0-3          -> sol. tot 4^3=64
+    //backtrack(0, n, arr);
+    return EXIT_SUCCESS;
 }
