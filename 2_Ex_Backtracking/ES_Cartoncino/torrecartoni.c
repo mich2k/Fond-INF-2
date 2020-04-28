4146811525ff4dcdb0942e37dd879d858af8e6e1
mich2k <miky3848@gmail.com>
2020-04-29 01:01:21 +0200

    latest

--- 2_Ex_Backtracking/ES_Cartoncino/torrecartoni.c
+++ 2_Ex_Backtracking/ES_Cartoncino/torrecartoni.c
@@ -1,8 +1,9 @@
 #include "torrecartoni.h"
 
-typedef struct{
+typedef struct
+{
     unsigned a;
-    int *c_ids; // Id cartoni --> 0, 2, 1 significa che la torre è costruita con i pacchi in 
+    int *c_ids; // Id cartoni --> 0, 2, 1 significa che la torre è costruita con i pacchi in
                 // posizione 0, 2 e 1 e in particolare il pacco 0 è in cima alla torre e quello
                 // di indice 1 si trova alla base
     int size;
@@ -10,12 +11,15 @@ typedef struct{
 
 void Backtracking(cartone *c, int n, int i, torre *vcurr, torre *vbest, bool *usati)
 {
-    if (i == n) {
-        for(int i = 0; i < n; ++i){
-           // printf("%i", vcurr->(c_idsi)+i);
+    if (i == n)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            // printf("%i", vcurr->(c_idsi)+i);
         }
         puts("");
-        if (vcurr->a > vbest->a) {
+        if (vcurr->a > vbest->a)
+        {
             vbest->a = vcurr->a;
             vbest->size = vcurr->size;
             memcpy(vbest->c_ids, vcurr->c_ids, vcurr->size * sizeof(int));
@@ -24,14 +28,18 @@ void Backtracking(cartone *c, int n, int i, torre *vcurr, torre *vbest, bool *us
         return;
     }
 
-    unsigned p = 0; 
-    for (int j = 0; j < vcurr->size; ++j) {
+    unsigned p = 0;
+    for (int j = 0; j < vcurr->size; ++j)
+    {
         p += c[vcurr->c_ids[j]].p;
     }
 
-    for (int j = 0; j < n; ++j) {
-        if (!usati[j]) {
-            if (p <= c[j].l) {
+    for (int j = 0; j < n; ++j)
+    {
+        if (!usati[j])
+        {
+            if (p <= c[j].l)
+            {
                 vcurr->a += c[j].a;
                 vcurr->c_ids[vcurr->size] = j;
                 vcurr->size++;
@@ -42,27 +50,25 @@ void Backtracking(cartone *c, int n, int i, torre *vcurr, torre *vbest, bool *us
                 vcurr->a -= c[j].a;
                 vcurr->size--;
                 usati[j] = 0;
-
             }
-            else {
+            else
+            {
                 Backtracking(c, n, n, vcurr, vbest, usati);
             }
         }
     }
-
-    
 }
-
 
 void TorreCartoni(cartone *c, int n)
 {
     bool *usati = calloc(n, sizeof(n));
-    torre vcurr = { .a = 0,.c_ids = malloc(n * sizeof(int)),.size = 0 };
-    torre vbest = { .a = 0,.c_ids = malloc(n * sizeof(int)),.size = 0 };
+    torre vcurr = {.a = 0, .c_ids = malloc(n * sizeof(int)), .size = 0};
+    torre vbest = {.a = 0, .c_ids = malloc(n * sizeof(int)), .size = 0};
 
     Backtracking(c, n, 0, &vcurr, &vbest, usati);
 
-    for (int i = 0; i < vbest.size; ++i) {
+    for (int i = 0; i < vbest.size; ++i)
+    {
         printf("%4i\n", vbest.c_ids[i]);
     }
     printf("Altezza: %i cm\n", vbest.a);
