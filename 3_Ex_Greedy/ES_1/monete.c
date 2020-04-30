d29e9991a622330ab20b837916ae90e687f23a19
mich2k <miky3848@gmail.com>
2020-04-30 03:24:47 +0200

    aggiunto esercizio monete, da finire

--- 3_Ex_Greedy/ES_1/monete.c
+++ 3_Ex_Greedy/ES_1/monete.c
@@ -1,19 +1,26 @@
+#include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h>
 
+int* Monete(int* t, int size, int b) {
+  if (b <= 0)
+    return NULL;
 
-int* Monete(int*t, int size, int b){
-
-
+  int a = 2;
+  for (uint32_t i = 0; i < size; ++i) {
+    int a;
+  }
+  ris[i] =
 }
 
-int main (void){
-    int v[] = {50,20,10,5,2,1};
-    int n = sizeof(v)/sizeof(*v);
-    int* ris = Monete(v, n, 2);
-    return EXIT_SUCCESS;
+int main(void) {
+  int v[] = {50, 20, 10, 5, 2, 1};
+  int n = sizeof(v) / sizeof(*v);
+  int* ris = Monete(v, n, 2);
+  return EXIT_SUCCESS;
 }
+#include <stdint.h>
 
+int a = 10;
 /*
     è necessario sia già ordinato in modo decrescente
     complessità da non ordinato O(nlogn)
