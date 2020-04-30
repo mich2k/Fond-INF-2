1e7df3c0410ff341fac6379693692246996af52d
mich2k <miky3848@gmail.com>
2020-04-30 22:46:39 +0200

    commit

--- 3_Ex_Greedy/ES_2/gioielli.c
+++ 3_Ex_Greedy/ES_2/gioielli.c
@@ -1,20 +1,27 @@
 #include "gioielli.h"
 
-bool readline(Gioiello* x, FILE* f) {
+Gioiello* read_line(FILE* f, bool* _eof) {
+    Gioiello* x = malloc(sizeof(Gioiello));
     uint32_t i;
-    uint32_t cont;
-    for (cont = 1;; cont++) {
+    uint32_t cont = 1;
+    while (true) {
         char isvalid = fgetc(f);
         if (cont != 1 && !(isprint(isvalid)) || (feof(f) || ferror(f))) {
             if (isvalid == '\n')
                 break;
-            else
-                fseek(f, -1, SEEK_CUR);
+            else if (feof(f) || ferror(f)) {
+                *_eof = true;
+                break;
+            }
         } else
             fseek(f, -1, SEEK_CUR);
         char* s = NULL;
         i = 0;
         for (; !isspace(fgetc(f)); ++i) {
+            if ((feof(f) || ferror(f))) {
+                *_eof = true;
+                break;
+            }
             fseek(f, -1, SEEK_CUR);
             s = realloc(s, i + 1);
             s[i] = fgetc(f);
@@ -32,17 +39,30 @@ bool readline(Gioiello* x, FILE* f) {
             default:
                 break;
         }
+        ++cont;
     }
+    return x;
+}
+
+Gioiello* Gioielli(const char* filename, float b, int* ret_size) {
+    FILE* f = fopen(filename, "r");
+    if (f == NULL)
+        exit(-1);
+    Gioiello x[];
+    bool _eof = false;
+    uint32_t i = 0;
+    while (_eof != true) {
+        x[i] = read_line(f, &_eof);
+        ++i;
+    }
+
+    return x;
 }
 
 int main(void) {
-    char* filename = "gioielli_1.inp";
-    FILE* f = fopen(filename, "r");
-    if (f == NULL)
-        return EXIT_FAILURE;
+    int retsize;
+    const char* filename = "gioielli_1.inp";
+    Gioielli(filename, (float)121, &retsize);
     // <codice><spazio><peso><spazio><prezzo><a capo>
-    Gioiello* x = malloc(sizeof(Gioiello));
-    readline(x, f);
-
     return EXIT_SUCCESS;
 }
