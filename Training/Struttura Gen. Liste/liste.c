b85aa318c90860eff43f2dadba90840a4630eb2d
mich2k <miky3848@gmail.com>
2020-05-15 19:14:26 +0200

    trying load

--- Training/Struttura Gen. Liste/liste.c
+++ Training/Struttura Gen. Liste/liste.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
+#include <ctype.h>
 #include "list_int.h"
 
-int main ( void ){
+Item* LoadList(const char*filename){
+    FILE* f = fopen(filename, "r");
+    if(f == NULL)
+        return NULL;
+    if(!inprint(fgetc(f))){
+        fclose(f);
+        return NULL;
+    }
     
+
 }
+
+int main ( void ){
+    char* filename = 
+}
+
+/*
+    
+
+*/
