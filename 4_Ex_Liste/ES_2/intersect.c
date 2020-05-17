5988286992e621886ea7dc2fbb7243e58487338c
mich2k <miky3848@gmail.com>
2020-05-18 01:26:43 +0200

    hot

--- 4_Ex_Liste/ES_2/intersect.c
+++ 4_Ex_Liste/ES_2/intersect.c
@@ -28,18 +28,16 @@ Item* Intersect(const Item* i1, const Item* i2) {
         return NULL;
     Item* ris_list = CreateEmptyList();
     Item* first_elem_pointer =(Item*) i2;
-    while (true) {
+    while (!IsEmptyList(i1)) {
         ElemType* first_elem = (int*)GetHeadValueList(i1);
         i2 = first_elem_pointer;
         while (!IsEmptyList(i2)) {
             ElemType* second_elem = (int*)GetHeadValueList(i2);
             if (ElemCompare(first_elem, second_elem) == 0)
-                ris_list = InsertHeadList((const ElemType)first_elem, ris_list);
+                ris_list = InsertHeadList(first_elem, ris_list);
             i2 = GetTailList(i2);
         }
         i1 = GetTailList(i1);
-        if (IsEmptyList(i1))
-            break;
     }
     WriteStdoutList(ris_list);
     return ris_list;
