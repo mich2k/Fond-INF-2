308b0bc9aa7e1ce9cf71f486a979308393ad1c46
mich2k <miky3848@gmail.com>
2020-05-18 01:22:24 +0200

    aggiunto ex insert

--- 4_Ex_Liste/ES_2/intersect.c
+++ 4_Ex_Liste/ES_2/intersect.c
@@ -1,5 +1,6 @@
-#include "list_int.h"
 #include <stdlib.h>
+#include <ctype.h>
+#include "list_int.h"
 
 Item* LoadList(const char* filename) {
     FILE* f = fopen(filename, "r");
@@ -22,20 +23,32 @@ Item* LoadList(const char* filename) {
     return list;
 }
 
-Item *Intersect (const Item* i1, const Item* i2){
-    if(IsEmptyList(i1) || IsEmptyList(i2))
+Item* Intersect(const Item* i1, const Item* i2) {
+    if (IsEmptyList(i1) || IsEmptyList(i2))
         return NULL;
-    Item* list = CreateEmptyList();
-    while(true){
-
+    Item* ris_list = CreateEmptyList();
+    Item* first_elem_pointer =(Item*) i2;
+    while (true) {
+        ElemType* first_elem = (int*)GetHeadValueList(i1);
+        i2 = first_elem_pointer;
+        while (!IsEmptyList(i2)) {
+            ElemType* second_elem = (int*)GetHeadValueList(i2);
+            if (ElemCompare(first_elem, second_elem) == 0)
+                ris_list = InsertHeadList((const ElemType)first_elem, ris_list);
+            i2 = GetTailList(i2);
+        }
+        i1 = GetTailList(i1);
+        if (IsEmptyList(i1))
+            break;
     }
-    //InsertBackList();
-
+    WriteStdoutList(ris_list);
+    return ris_list;
 }
 
-int main(void){
-    Item* first = LoadList("/home/mich/Scrivania/UNI/Fond-INF-2/4_Ex_Liste/ES_2/data_00.inp");
-    Item* second = LoadList("/home/mich/Scrivania/UNI/Fond-INF-2/4_Ex_Liste/ES_2/data_01.inp");
+int main(void) {
+    Item* first = LoadList(
+        "/home/mich/Scrivania/UNI/Fond-INF-2/4_Ex_Liste/ES_2/data_00.inp");
+    Item* second = LoadList(
+        "/home/mich/Scrivania/UNI/Fond-INF-2/4_Ex_Liste/ES_2/data_01.inp");
     Intersect(first, second);
-
 }
