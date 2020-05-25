d8950db22b76b916247acb177f2ba6306f3575eb
mich2k <miky3848@gmail.com>
2020-05-25 08:30:15 +0200

    main and input are working

--- ESAMI/SIM-VINCINI/ES_1/cena.c
+++ ESAMI/SIM-VINCINI/ES_1/cena.c
@@ -17,34 +17,42 @@ typedef struct {
     char nome[20];
 } piatto;
 
-void backtrack(piatto x, size_t n, size_t i, size_t k, uint32_t* vcurr) {
+typedef uint32_t u32;
+
+void backtrack(piatto* arr, size_t n, size_t i, size_t k, u32* vcurr) {
     if (i == n) {
-        for (uint32_t j = 0; j < n; ++j)
-            printf("%d ", vcurr[j]);
+        // for (u32 j = 0; j < n; ++j)
+        //    printf("%d ", vcurr[j]);
+        u32 kcal_sum = 0;
+        for (u32 j = 0; j < n; j += 1) {
+            // if(vcurr[j] == true)
+            fprintf(stdout, "%d %d %s \n", arr[j].tipologia, arr[j].Kcal,
+                    arr[j].nome);
+        }
         puts("");
         return;
     }
-    for (uint32_t r = 0; r < k; ++r) {
+    for (u32 r = 0; r < k; ++r) {
         vcurr[i] = r;
-        backtrack(x, n, i + 1, k, vcurr);
+        backtrack(arr, n, i + 1, k, vcurr);
     }
 }
 
 int main(void) {
-    size_t k = 2;  // binario
-    size_t N = 6;  // quante terne di piatti;
-    size_t n_piatti = 3;
-    uint32_t* vcurr = malloc(N * 32);
+    size_t k = 2;         // binario
+    size_t N = 2;         // quante terne di piatti;
+    size_t n_piatti = 3;  // numero piatti per ogni terna
+    u32* vcurr = malloc(n_piatti * sizeof(u32));
 
     // array struct
 
-    piatto arr[] = {
-        {1, 200, "primo"}, {2, 300, "secondo"}, {3, 150, "terzo"},
-        {1, 200, "primo"}, {2, 300, "secondo"}, {3, 150, "terzo"}
-    };
+    piatto arr[] = {{1, 200, "primo"}, {2, 300, "secondo"}, {3, 150, "terzo"},
+                    {1, 200, "primo"}, {2, 300, "secondo"}, {3, 150, "terzo"}};
 
-    for (size_t r = 0; r < N; r += 1)
-        fprintf(stdout, "%d %d %s \n", arr[r].tipologia, arr[r].Kcal, arr[r].nome);
-
-    // backtrack(x, n_piatti, 0, 2, vcurr);
+    /*for (size_t r = 0; r < N * n_piatti; r += 1)
+        fprintf(stdout, "%d %d %s \n", arr[r].tipologia, arr[r].Kcal,
+                arr[r].nome);
+    */
+    backtrack(arr, n_piatti, 0, 2, vcurr);
+    return EXIT_SUCCESS;
 }
