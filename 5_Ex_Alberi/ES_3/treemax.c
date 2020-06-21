505bdb4f9892119ece9aa58a92976c896e2ff076
Michele G <michele.g2k1@gmail.com>
2020-06-21 22:53:28 +0200

    bstmax added, working

---
+++ 5_Ex_Alberi/ES_3/treemax.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include "tree_int.h"
+
+extern const ElemType* BstTreeMax(const Node*);
+
+const ElemType* BstTreeMax(const Node* n) {
+    if(IsEmptyTree(n))
+        return NULL;
+    ElemType* max;
+    while(!IsLeafTree(n)){
+        n=RightTree(n);
+        max = (ElemType*)GetRootValueTree(n);
+    }
+    return max;
+}
+
+int main(void) {
+    ElemType v[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Node* tree = CreateRootTree(
+        &v[6],
+        CreateRootTree(
+            &v[2], CreateRootTree(&v[3], CreateEmptyTree(), CreateEmptyTree()),
+            CreateRootTree(&v[9], CreateEmptyTree(), CreateEmptyTree())),
+        CreateRootTree(
+            &v[3], CreateRootTree(&v[4], CreateEmptyTree(), CreateEmptyTree()),
+            CreateRootTree(&v[3], CreateEmptyTree(), CreateEmptyTree())));
+    ElemType max = *BstTreeMax(tree);
+    return EXIT_SUCCESS;
+}
--- Training/Algoritmo_Gen_Stampa_Albero/gen_tree.c
+++ Training/Algoritmo_Gen_Stampa_Albero/gen_tree.c
@@ -15,6 +15,18 @@ void ricorsione(Node* n) {
     ricorsione(RightTree(n));
 }
 
+//  ALGORITMO ESPLORAZIONE ALBERO INTERO:
+
+/*
+        SE L ALBERO E VUOTO
+            RITORNA
+
+        RICORSIONE (LEFT)
+
+        RICORSIONE (RIGHT)
+*/
+
+
 int main(void) {
     ElemType v[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     Node* tree = CreateRootTree(
