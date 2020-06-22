4ca96ed112078cb1c85cd627d57ce2bb67340f27
Michele G <michele.g2k1@gmail.com>
2020-06-22 16:35:59 +0200

    init filtra

---
+++ ESAMI/SIMULAZIONI_LAB/ESAME_1/ES_2/filtra.c
@@ -0,0 +1,54 @@
+
+/*
+Nel file filtra.c definire la funzione corrispondente alla seguente dichiarazione: 
+
+extern list Filtra(list l, const char *citta);
+
+La funzione prende in input una lista di indirizzi (anche vuota) e una citta e costruisce
+una nuova lista contenente tutti gli indirizzi che non fatto parte di quella città. Gli 
+elementi della lista di output devono essere nello stesso ordine di quella di input. Se la 
+lista è una lista vuota la funzione deve ritornare NULL. N.B. quando fate la sottomissione
+della soluzione non dovete caricare il codice delle primitive, ma solo il codice della 
+funzione Filtra e di eventuali funzioni ausiliarie.
+
+Per la risoluzione di questo esercizio avete a disposizione le seguenti definizioni:
+
+typedef struct indirizzo {
+  char via[50]; int civico; char citta[30];
+} indirizzo;
+
+typedef indirizzo element; 
+
+typedef struct list_element {
+  element value;
+  struct list_element *next;
+} item;
+
+typedef item* list; 
+
+e le seguenti primitive:
+
+list EmptyList();
+list Cons(const element *e, list l);
+bool IsEmpty(list l);
+element Head(list l);
+list Tail(list l);
+element Copy(const element *e);
+void FreeList(list l);
+list InsertBack(list l, const element *e);*/
+
+#include "list_int.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+extern list Filtra(list, const char*);
+
+
+list Filtra(list l, const char *citta){
+    
+}
+
+
+int main(void){
+
+}
