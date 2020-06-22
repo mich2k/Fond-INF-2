0bea19d544963ab191af74e12b78731ec865d611
Michele G <michele.g2k1@gmail.com>
2020-06-22 15:24:51 +0200

    init ex lab

---
+++ ESAMI/SIMULAZIONI_LAB/ESAME_1/inverti.c
@@ -0,0 +1,28 @@
+/*
+Nel file inverti.c definire la funzione corrispondente alla seguente
+dichiarazione: 
+
+extern void Inverti(int *v, int v_size); 
+
+La funzione accetta un vettore di interi (v), anche vuoto, e la sua 
+dimensione(v_size) e deve scambiare ricorsivamente gli elementi di 
+v per posizionarli in ordine inverso rispetto alla posizione iniziale.  
+Se, ad esempio, il vettore v contenesse gli elementi 0 1 2 3 4 5, al 
+termine dell’esecuzione della funzione Inverti dovrebbe contenere gli 
+elementi 5 4 3 2 1 0. Si consiglia l’utilizzo di una funzione ausiliaria 
+per realizzare la ricorsione. N.B. Non sarà considerata valida nessuna 
+soluzione che non faccia uso della ricorsione per scorrere gli elementi
+del vettore.
+*/
+
+extern void Inverti(int *v, int v_size); 
+
+void Inverti(int *v, int v_size){
+
+
+}
+
+int main (void){
+
+
+}
