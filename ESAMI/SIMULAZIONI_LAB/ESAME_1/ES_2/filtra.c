2ee9d4ec8c07d93ec34d6bf2919b38d82cacc66c
Michele G <michele.g2k1@gmail.com>
2020-06-22 16:57:13 +0200

    init fitra

--- ESAMI/SIMULAZIONI_LAB/ESAME_1/ES_2/filtra.c
+++ ESAMI/SIMULAZIONI_LAB/ESAME_1/ES_2/filtra.c
@@ -1,30 +1,33 @@
 
 /*
-Nel file filtra.c definire la funzione corrispondente alla seguente dichiarazione: 
+Nel file filtra.c definire la funzione corrispondente alla seguente
+dichiarazione:
 
 extern list Filtra(list l, const char *citta);
 
-La funzione prende in input una lista di indirizzi (anche vuota) e una citta e costruisce
-una nuova lista contenente tutti gli indirizzi che non fatto parte di quella città. Gli 
-elementi della lista di output devono essere nello stesso ordine di quella di input. Se la 
-lista è una lista vuota la funzione deve ritornare NULL. N.B. quando fate la sottomissione
-della soluzione non dovete caricare il codice delle primitive, ma solo il codice della 
-funzione Filtra e di eventuali funzioni ausiliarie.
+La funzione prende in input una lista di indirizzi (anche vuota) e una citta e
+costruisce una nuova lista contenente tutti gli indirizzi che non fatto parte di
+quella città. Gli elementi della lista di output devono essere nello stesso
+ordine di quella di input. Se la lista è una lista vuota la funzione deve
+ritornare NULL. N.B. quando fate la sottomissione della soluzione non dovete
+caricare il codice delle primitive, ma solo il codice della funzione Filtra e di
+eventuali funzioni ausiliarie.
 
-Per la risoluzione di questo esercizio avete a disposizione le seguenti definizioni:
+Per la risoluzione di questo esercizio avete a disposizione le seguenti
+definizioni:
 
 typedef struct indirizzo {
   char via[50]; int civico; char citta[30];
 } indirizzo;
 
-typedef indirizzo element; 
+typedef indirizzo element;
 
 typedef struct list_element {
   element value;
   struct list_element *next;
 } item;
 
-typedef item* list; 
+typedef item* list;
 
 e le seguenti primitive:
 
@@ -37,18 +40,37 @@ element Copy(const element *e);
 void FreeList(list l);
 list InsertBack(list l, const element *e);*/
 
-#include "list_int.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "list_int.h"
+
+typedef struct indirizzo {
+    char via[50];
+    int civico;
+    char citta[30];
+} indirizzo;
+
+typedef indirizzo element;
+
+typedef struct list_element {
+    element value;
+    struct list_element* next;
+} item;
 
 extern list Filtra(list, const char*);
 
+list Filtra(list l, const char* citta) {
 
-list Filtra(list l, const char *citta){
+    if(IsEmptyList(l))
+        return NULL;
     
+
 }
 
-
-int main(void){
+int main(void) {
+    indirizzo primo = {"via gennaro", 69, "Napoli"};
+    indirizzo secondo = {"via esposito", 69, "Modena"};
+    indirizzo terzo = {"via ", 69, "Bolo"};
 
 }
