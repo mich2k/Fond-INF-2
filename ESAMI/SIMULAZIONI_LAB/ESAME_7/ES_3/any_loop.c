35ceb6c1a762a0b4e015b5bce7f3709435338642
Michele G <miky3848@gmail.com>
2020-07-02 17:17:09 +0200

    update

--- ESAMI/SIMULAZIONI_LAB/ESAME_7/ES_3/any_loop.c
+++ ESAMI/SIMULAZIONI_LAB/ESAME_7/ES_3/any_loop.c
@@ -10,7 +10,6 @@ Una lista contiene un ciclo se almeno uno dei suoi elementi è raggiungibile da 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-cai
 
 extern bool AnyLoop(Item* l);
 
