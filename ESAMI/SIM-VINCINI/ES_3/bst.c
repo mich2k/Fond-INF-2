867ff7f70eaf6c122121dfb3a183f24f302cde0a
Michele G <miky3848@gmail.com>
2020-07-17 21:25:51 +0200

    bst

--- ESAMI/SIM-VINCINI/ES_3/bst.c
+++ ESAMI/SIM-VINCINI/ES_3/bst.c
@@ -2,24 +2,31 @@
 #include <stdlib.h>
 #include "tree_int.h"
 
-bool rec(Node* t){
-    if(IsLeafTree(t))
+void rec(Node* t, bool* ret) {
+    if(IsLeafTree(t) || *ret == false)
         return;
-    printf("%d ", *GetRootValueTree(t));
-    if(!(t->left->value <= t->value && t->right->value > t->value ))
-        return false;
-    rec(LeftTree(t));
-    rec(RightTree(t));
-    return true;
+    //printf("%d ", *GetRootValueTree(t));
+    if (!IsEmptyTree(t->left)) {
+        if (!(t->left->value <= t->value))
+            *ret = false;
+    }
+    if (!IsEmptyTree(t->right)) {
+        if (!(t->right->value > t->value))
+            *ret = false;
+    }
+    rec(LeftTree(t),ret);
+    rec(RightTree(t),ret);
 }
 
 bool bst_check(Node* t) {
-    return rec(t);
+    bool ret = true;
+    rec(t, &ret);
+    return ret;
 }
 
 int main(void) {
     ElemType v[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    Node* tree = CreateRootTree(    // albero NON BST
+    Node* tree = CreateRootTree(  // albero NON BST
         &v[6],
         CreateRootTree(
             &v[2], CreateRootTree(&v[7], CreateEmptyTree(), CreateEmptyTree()),
@@ -27,8 +34,18 @@ int main(void) {
         CreateRootTree(
             &v[3], CreateRootTree(&v[4], CreateEmptyTree(), CreateEmptyTree()),
             CreateRootTree(&v[3], CreateEmptyTree(), CreateEmptyTree())));
-    printf("\n--> %d\n", bst_check(tree));
+    Node* bst_tree = CreateRootTree(  // albero NON BST
+        &v[6],
+        CreateRootTree(
+            &v[2], CreateRootTree(&v[1], CreateEmptyTree(), CreateEmptyTree()),
+            CreateRootTree(&v[9], CreateEmptyTree(), CreateEmptyTree())),
+        CreateRootTree(
+            &v[7], CreateRootTree(&v[6], CreateEmptyTree(), CreateEmptyTree()),
+            CreateRootTree(&v[9], CreateEmptyTree(), CreateEmptyTree())));
     WriteStdoutInOrderTree(tree);
+    printf("\n--> %d\n", bst_check(tree));
+    WriteStdoutInOrderTree(bst_tree);
+    printf("\n--> %d\n", bst_check(bst_tree));
     DeleteTree(tree);
     return EXIT_SUCCESS;
 }
