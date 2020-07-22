241bbee9f901a728b71d4c8ad1eabcb3e9311b5d
Michele G <miky3848@gmail.com>
2020-07-22 12:21:09 +0200

    updsted

--- Training/Combinazioni/comb.c
+++ Training/Combinazioni/comb.c
@@ -4,61 +4,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void combinationUtil(int arr[],
-                     int data[],
-                     int start,
-                     int end,
-                     int index,
-                     int r,
-                     size_t* cont);
+void comb_backtrack(const int* arr,
+                    int* vcurr,
+                    int start,
+                    int end,
+                    int index,
+                    int r,
+                    size_t* cont);
 
 // The main function that prints all combinations of size r
-// in arr[] of size n. This function mainly uses combinationUtil()
-void printCombination(int arr[], int n, int k, size_t* cont) {
-    int *data=malloc(n* sizeof(int));
-
-    // Print all combination using temprary array 'data[]'
-    combinationUtil(arr, data, 0, n - 1, 0, k, cont);
-    free(data);
+// in arr[] of size n. This function mainly uses comb_backtrack()
+void print_combination(int n, int k, size_t* cont) {
+    int* vcurr = malloc(k * sizeof(int));
+    int* digits = malloc(n * sizeof(int));
+    for (size_t arr_cont = 0; arr_cont < n; arr_cont += 1)
+        digits[arr_cont] = arr_cont + 1;
+    // Print all combination using temprary array 'vcurr[]'
+    comb_backtrack(digits, vcurr, 0, n - 1, 0, k, cont);
+    free(vcurr);
+    free(digits);
 }
 
-/* arr[]  ---> Input Array
-   data[] ---> Temporary array to store current combination
+/* arr[]  ---> Digit Input Array
+   vcurr[] ---> Temporary array to store current combination
    start & end ---> Staring and Ending indexes in arr[]
-   index  ---> Current index in data[]
+   index  ---> Current index in vcurr[]
    r ---> Size of a combination to be printed */
-void combinationUtil(int arr[],
-                     int data[],
-                     int start,
-                     int end,
-                     int i,
-                     int k,
-                     size_t* cont) {
+void comb_backtrack(const int* arr,
+                    int* vcurr,
+                    int start,
+                    int end,
+                    int i,
+                    int k,
+                    size_t* cont) {
     // Current combination is ready to be printed, print it
     if (i == k) {
         (*cont)++;
         printf("%zu)\t", *cont);
         for (int j = 0; j < k; j++)
-            printf("%d ", data[j]);
+            printf("%d ", vcurr[j]);
         printf("\n");
         return;
     }
 
-    // replace i with all possible elements. The condition
-    // "end-i+1 >= r-i" makes sure that including one element
-    // at index will make a combination with remaining elements
-    // at remaining positions
-    for (int j = start; j <= end && end - j + 1 >= k - i; j+=1) {
-        data[i] = arr[j];
-        combinationUtil(arr, data, j + 1, end, i + 1, k, cont);
+    for (int j = start; j <= end && end - j + 1 >= k - i; j += 1) {
+        vcurr[i] = arr[j];
+        comb_backtrack(arr, vcurr, j + 1, end, i + 1, k, cont);
     }
 }
 
 // Driver program to test above functions
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6};
+    // int arr[] = {1, 2, 3, 4, 5, 6};
     int k = 3;
+    int n = 6;
     size_t cont = 0;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printCombination(arr, n, k, &cont);
+    print_combination(n, k, &cont);
+    return EXIT_SUCCESS;
 }
